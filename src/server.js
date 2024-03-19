1f5b80fbca682068e7c06fd5d684e92df78153c8
mbizissac <148372207+mbizissac@users.noreply.github.com>
2024-03-19 15:48:38 +0700

    xxx

--- src/server.js
+++ src/server.js
@@ -1,11 +1,28 @@
 const express = require('express') //import express
+const path = require('path'); //commonjs
+require('dotenv').config();
+
 const app = express() // tạo express application
-const port = 3000 // init port
+const port = process.env.PORT||8888; // init port
+const hostname = process.env.HOSTNAME;
+
+//config template engine
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'ejs')
+
 //khai báo routes
 //req (request), res(response) là 2 object trong môi trường Node.js
 app.get('/', (req, res) => {
  res.send('Hello World! xae ')
 })
+
+app.get('/hehe', (req, res) => {
+    res.send('Hello World! qqqqq q   qq qxae ')
+})
+app.get('/abc', (req, res) => {
+    res.render('sample.ejs')
+})
+
 //run server trên port đã khởi tạo trước đấy
 //nạp các thông tin khai báo ở trên rồi chạy (ví dụ như nạp routes)
 app.listen(port, () => {
